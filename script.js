2580f47fb0f61d0837701d446eb25e05e21b9e66
singlertam <alarkhanov.ma@gmail.com>
2023-11-01 23:38:49 +0300

    update

--- script.js
+++ script.js
@@ -1,69 +1,69 @@
-// // 1 Задание 
-// let a = 10;
-// alert(a);
-// a = 20;
-// alert(a);
+// 1 Задание 
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
 
-// // 2 Задание 
-// let release = 2007;
-// alert(release);
+// 2 Задание 
+let release = 2007;
+alert(release);
 
-// // 3 Задание 
-// let inventor = 'Brendan Eich';
-// alert(inventor);
+// 3 Задание 
+let inventor = 'Brendan Eich';
+alert(inventor);
 
-// // 4 Задание 
-// let one = 10;
-// let two = 2;
+// 4 Задание 
+let one = 10;
+let two = 2;
 
-// alert(`Сумма: ${one + two}, Разность: ${one - two}, Произведение: ${one * two}, Частное:${10 % 2} `);
+alert(`Сумма: ${one + two}, Разность: ${one - two}, Произведение: ${one * two}, Частное:${10 % 2} `);
 
-// // 5 Задание
-// let degreeOne = 2;
-// let degreeTwo = 5;
+// 5 Задание
+let degreeOne = 2;
+let degreeTwo = 5;
 
-// let result = degreeOne ** degreeTwo;
-// alert(result);
+let result = degreeOne ** degreeTwo;
+alert(result);
 
-// // 6 Задание 
-// let b = 9;
-// let c = 2;
+// 6 Задание 
+let b = 9;
+let c = 2;
 
-// alert(b % c);
+alert(b % c);
 
-// // 7 Задание 
-// let num = 1;
-// num += 5;
-// num -= 3;
-// num *= 7;
-// num /= 3;
-// ++num;
-// --num;
-// alert(num);
+// 7 Задание 
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+++num;
+--num;
+alert(num);
 
-// // 8 Задание 
-// let age = prompt("Сколько вам лет?");
-// alert(parseInt(age));
+// 8 Задание 
+let age = prompt("Сколько вам лет?");
+alert(parseInt(age));
 
-// // 9 Задание
-// let user = {
-//     name: 'Azim',
-//     age: 21,
-//     isAdmin: true
-// }
+// 9 Задание
+let user = {
+    name: 'Azim',
+    age: 21,
+    isAdmin: true
+}
 
-// user.cityOfResidence = 'Grozny';
-// user.age = 25;
+user.cityOfResidence = 'Grozny';
+user.age = 25;
 
-// delete user.cityOfResidence;
+delete user.cityOfResidence;
 
-// let info = prompt("Какую информацию хотите узнать о пользователе?");
+let info = prompt("Какую информацию хотите узнать о пользователе?");
 
-// alert(user[info]);
+alert(user[info]);
 
-// // 10 Задание
-// let data = prompt('Как вас зовут?');
+// 10 Задание
+let data = prompt('Как вас зовут?');
 
-// alert(`Привет, ${data}!`);
+alert(`Привет, ${data}!`);
 
 
