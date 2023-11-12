8a731631c486a1a26f0b02934e259e0ce7f26e0b
singlertam <alarkhanov.ma@gmail.com>
2023-11-12 14:09:59 +0300

    hw-4

--- script.js
+++ script.js
@@ -1,110 +1,53 @@
 // Задание 1
 
-// let password = 'world';
-// let request = prompt('Введите пароль');
+// let i = 0;
 
-// // if (password === request) {
-// //     alert('Пароль введен верно');
-// // } else {
-// //     alert('Пароль введен неверно');
-// // }
-
-// (password === request) ? alert('Пароль введен верно') : alert('Пароль введен неверно');
-
-// // Задание 2 
-
-// let c = 11;
-
-// if (c > 0 && c < 10) {
-//     console.log('Верно');
-// } else {
-//     console.log('Неверно');
+// while (i < 2) {
+//     console.log('Привет!');
+//     i++;
 // }
 
-// // Задание 3 
+// Задание 2
 
-// let d = 5;
-// let e = 3;
-
-// if (d > 100 || e > 100) {
-//     console.log('Верно');
-// } else {
-//     console.log('Неверно');
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
 // }
 
-// // Задание 4 
+// Задание 3
 
-// let a = '2';
-// let b = '3';
-// // Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
-// alert(parseInt(a) + parseInt(b));
-
-// // Задание 5 
-
-// let monthNumber = 13;
-
-// switch (monthNumber) {
-//     case 1:
-//     case 2:
-//     case 12:
-//         console.log('Зима');
-//         break;
-//     case 3:
-//     case 4:
-//     case 5:
-//         console.log('Весна');
-//         break;
-//     case 6:
-//     case 7:
-//     case 8:
-//         console.log('Лето');
-//         break;
-//     case 9:
-//     case 10:
-//     case 11:
-//         console.log('Осень');
-//         break;
-//     default:
-//         сonsole.log('Такого месяца не существует')
+// for (let i = 7; i <= 22; i++) {
+//     console.log(i);
 // }
 
-// Задание 6
+// Задание 4
 
-// let evenOrNot = prompt('Пожалуйста, введите любое число');
+// let salary = {
+//     "Коля" : "200",
+//     "Вася" : "300",
+//     "Петя" : "400"
+// };
 
-// let userNumber = parseFloat(evenOrNot);
-
-// if (!isNaN(userNumber)) {
-//     if (userNumber % 2 === 0) {
-//         alert("Число четное");
-//     } else {
-//         alert("Число нечетное");
-//     }
-// } else {
-//     alert("Вы ввели некорректное значение. Пожалуйста, введите число.");
+// for (let key in salary) {
+//     console.log(`${key} — зарплата ${salary[key]} долларов.`)
 // }
 
-// Задание 7 
+// Задание 5
 
-// let clientOs = 0;
+// let n = 1000;
+// let num = 0;
 
-// if (clientOs === 0) {
-//     console.log('Установите версию приложения для iOS по ссылке');
-// } else if (clientOs === 1) {
-//     console.log('Установите версию приложения для Android по ссылке');
-// } else {
-//     console.log('Неизвестная операционная система');
-// }
+// do {
+//     n = n / 2;
+//     num++;
+// } while (n >= 50);
 
-// Задание 8 
+// console.log("Полученное число:", n);
+// console.log("Количество итераций:", num);
 
-// let clientOs = 0;
-// let clientDeviceYear = 2015;
+// Задание 6 
 
-// if (clientDeviceYear < 2015 && clientOs === 0) {
-//     console.log('Установите облегченную версию приложения для iOS по ссылке');
-// } else if (clientDeviceYear < 2015 && clientOs === 1) {
-//     console.log('Установите облегченную версию приложения для Android по ссылке');
-// } else {
-//     console.log('У вас актуальная операционная система');
-// }
+let firstFriday = 5;
+
+for (let date = firstFriday; date <= 31; date += 7) {
+    console.log(`Сегодня пятница, ${date}-е число. Необходимо подготовить отчет.`);
+}
