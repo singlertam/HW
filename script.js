4b31d3f0f43e508a6457aaa59dcc4996c22a0bdb
singlertam <alarkhanov.ma@gmail.com>
2023-11-18 11:17:16 +0300

    hw-5 done

--- script.js
+++ script.js
@@ -1,53 +1,152 @@
-// Задание 1
+// Задание 1 
 
-// let i = 0;
+// const checkLeast = (value1, value2) => {
+//     let a = parseFloat(prompt(value1));
+//     let b = parseFloat(prompt(value2));
 
-// while (i < 2) {
-//     console.log('Привет!');
-//     i++;
+//     if (isNaN(a) || isNaN(b)) {
+//         console.log("Введите корректные числовые значения");
+//         return;
+//     }
+
+//     if (a > b) {
+//         console.log(`Значение ${a} больше второго`);
+//     } else if (a < b) {
+//         console.log(`Значение ${b} больше первого`);
+//     } else {
+//         console.log("Значения равны");
+//     }
 // }
 
-// Задание 2
+// checkLeast("Введите два значения для их сравнения");
 
-// for (let i = 1; i <= 5; i++) {
-//     console.log(i);
+// Задание 2 
+
+// const checkEven = (request) => {
+//     let userNumber = prompt(request);
+
+//     if (isNaN(userNumber)) {
+//         console.log("Введите корректное числовое значение");
+//         return;
+//     }
+
+//     if (userNumber % 2 === 0) {
+//         console.log(`Значение ${userNumber} является четным`);
+//     } else {
+//         console.log(`Значение ${userNumber} является нечетным`);
+//     }
 // }
 
-// Задание 3
+// checkEven("Введите значение, чтобы выяснить четное это значение или нечетное");
 
-// for (let i = 7; i <= 22; i++) {
-//     console.log(i);
+// Задание 3.1
+
+// const makeSquare = (userAnswer) => {
+//     let square = prompt(userAnswer);
+
+//     console.log(square ** 2);
 // }
 
-// Задание 4
+// makeSquare('Введите значение, чтобы возвести в квадрат');
 
-// let salary = {
-//     "Коля" : "200",
-//     "Вася" : "300",
-//     "Петя" : "400"
-// };
+// Задание 3.2
 
-// for (let key in salary) {
-//     console.log(`${key} — зарплата ${salary[key]} долларов.`)
+// const returnSquare = (userAnswer) => {
+//     let square = prompt(userAnswer);
+
+//     return alert(square ** 2);
 // }
 
-// Задание 5
+// returnSquare('Введите значение, чтобы возвести в квадрат');
 
-// let n = 1000;
-// let num = 0;
+// Задание 4 
 
-// do {
-//     n = n / 2;
-//     num++;
-// } while (n >= 50);
+// const checkAge = (userAnswer) => {
+//     let age = prompt(userAnswer);
 
-// console.log("Полученное число:", n);
-// console.log("Количество итераций:", num);
+//     if (age < 0) {
+//         alert("Вы ввели неправильное значение");
+//     } else if (age > 0 && age < 12) {
+//         alert("Привет, друг!");
+//     } else {
+//         alert("Добро пожаловать!")
+//     }
+// }
+
+// checkAge("Укажите сколько вам лет?");
+
+// Задание 5 
+
+// const check = (valueOne, valueTwo) => {
+//     let a = parseFloat(prompt(valueOne));
+//     let b = parseFloat(prompt(valueTwo));
+    
+//     if (isNaN(a) || isNaN(b)) {
+//         return "Одно или оба значения не являются числом";
+//     } else {
+//         return a * b;
+//     }
+// }
+
+// console.log(check("Введите значения для проверки"));
 
 // Задание 6 
 
-let firstFriday = 5;
+// const getCube = (request) => {
+//     let userNumber = prompt(request);
 
-for (let date = firstFriday; date <= 31; date += 7) {
-    console.log(`Сегодня пятница, ${date}-е число. Необходимо подготовить отчет.`);
-}
+//     if (isNaN(userNumber)) {
+//         return "Переданный параметр не является числом";
+//     } else {
+//         return userNumber ** 3;
+//     }
+// };
+
+// console.log(getCube("Введите значение, чтобы возвести его в куб"));
+
+// Задание 7 
+
+// const getArea = function() {
+//     const p = 3.14;
+//     return `Площадь круга составляет ${p * (this.radius ** 2)}`;
+// }
+
+// const getPerimetr = function() {
+//     const p = 3.14;
+//     return `Периметр круга составляет ${2 * p * this.radius}`;
+// }
+
+// const circle1 = {
+//     radius: 5,
+//     getArea: getArea,
+//     getPerimetr: getPerimetr
+// }
+
+// const circle2 = {
+//     radius: 7,
+//     getArea: getArea,
+//     getPerimetr: getPerimetr
+// }
+
+// console.log(circle1.getArea());
+// console.log(circle1.getPerimetr());
+// console.log(circle2.getArea());
+// console.log(circle2.getPerimetr());
+
+// Задание 8
+
+const seasonsGame = (userAnswer) => {
+    let answer = parseInt(prompt(userAnswer));
+
+    if (isNaN(answer) || answer < 1 || answer > 12) {
+        return "Вы ввели некорректное значение, попробуйте снова!";
+    } else if(answer === 12 || answer === 1 || answer === 2) {
+        return "Зима";
+    } else if(answer >= 3 && answer <= 5) {
+        return "Весна";
+    } else if(answer >= 6 && answer <= 8) {
+        return "Лето";
+    } else if(answer >= 9 && answer <= 11) {
+        return ("Осень");
+    } 
+};
